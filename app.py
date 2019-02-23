2e619ca0e633b0de9acf816b803bfc17f4f7fe6a
Nara Lee <daisyleenr@gmail.com>
2019-02-23 15:01:24 +0900

    API CRUD 추가

--- app.py
+++ app.py
@@ -1,54 +1,74 @@
 # coding: utf-8
 
-from flask import Flask, Response
+from flask import Flask, Response, request
 # import ujson as json
 import json
+
+from flask_cors import CORS
+
+
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-
-# class A:
-#     def __init__(self):
-#         self._a = 10
-#
-# class B:
-#     def __init__(self):
-#         self._a = 20
-#
-# s = A()
-# s1 = B()
-#
-# def create_class(meta_class):
-#     return meta_class()
-#
-# s2 = create_class(A)
-# s3 = create_class(B)
-
-
-@app.route("/list")
-def hello():
-
-    todos = [
-      {
+todos = [
+    {
         'id': 0,
         'text': '앵귤러 배우고',
         'checked': True,
-      },
-      {
+    },
+    {
         'id': 1,
         'text': '리액트 배우고',
         'checked': False,
-      },
-      {
+    },
+    {
         'id': 2,
         'text': '뷰 배우자',
         'checked': False,
-      },
-    ]
+    },
+]
+
+
+@app.route("/todos")
+def get_todos():
+    resp = Response(response=json.dumps(todos),
+                    mimetype="application/json") #Response 객체 생성
+    return resp
+
+
+@app.route("/todos", methods=['POST'])
+def add_todo():
+    is_checked = request.form['check'] == 'true'
+    value = {'text': request.form['text'], 'checked': is_checked, 'id': len(todos)}
+    todos.append(value)
+
+    resp = Response(response=json.dumps(value),
+                    mimetype="application/json") #Response 객체 생성
+    return resp
+
+
+@app.route("/todos/<int:todo_id>", methods=['PUT'])
+def update_todo(todo_id):
+    for todo in todos:
+        if todo['id'] == todo_id:
+            todo['checked'] = not todo['checked']
+            break
+
+    resp = Response(response=json.dumps(todos),
+                    mimetype="application/json")  # Response 객체 생성
+    return resp
+
+
+@app.route("/todos/<int:todo_id>", methods=['DELETE'])
+def remove_todo(todo_id):
+    for idx, todo in enumerate(todos):
+        if todo['id'] == todo_id:
+            # todos.remove(todo)
+            todos.pop(idx)
+            break
 
     resp = Response(response=json.dumps(todos),
                     mimetype="application/json") #Response 객체 생성
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-
     return resp
 
 
