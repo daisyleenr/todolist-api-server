194e7600c76712b5ee7be5924fa797561f4ecebb
Nara Lee <daisyleenr@gmail.com>
2019-02-26 23:18:56 +0900

    sqlalchemy 연동

--- app.py
+++ app.py
@@ -1,86 +1,69 @@
 # coding: utf-8
 
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 # import ujson as json
-import json
+# import json
 from flask_cors import CORS
-import MySQLdb
-from config import *
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from model import *
+from config import TEST_DB_STRING
+from marshmallow import Schema, fields
+
+engine = create_engine(TEST_DB_STRING, echo=True, pool_recycle=3600)
+Session = sessionmaker(bind=engine)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-def get_db_connect():
-    db = MySQLdb.connect(user=DB_USER, passwd=DB_PASS, db=DB_NAME)
-    return db
+
+class TodoSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    checked = fields.Boolean()
+    created_at = fields.DateTime()
+
+
+todos_schema = TodoSchema(many=True)
+
 
 @app.route("/todos")
 def get_todos():
-    db = get_db_connect()
-    c = db.cursor(MySQLdb.cursors.DictCursor)
-    c.execute("""select id, todo as text, checked, UNIX_TIMESTAMP(created_at) as created_at from todos""")
-    todos = c.fetchall()
-    c.close()
-
-    resp = Response(response=json.dumps(todos),
-                    mimetype="application/json") #Response 객체 생성
-    return resp
+    todos = db_session.query(Todos).all()
+    return jsonify(todos_schema.dump(todos).data)
 
 
 @app.route("/todos", methods=['POST'])
 def add_todo():
-    db = get_db_connect()
-    c = db.cursor(MySQLdb.cursors.DictCursor)
-
     todo_text = request.form['text']
-    c.execute(f"""insert into todos(todo) values('{todo_text}')""")
-    db.commit()
+    db_session.add(Todos(title=todo_text))
+    db_session.commit()
 
-    c.execute("""select id, todo as text, checked, UNIX_TIMESTAMP(created_at) as created_at from todos""")
-    todos = c.fetchall()
-    c.close()
-
-    resp = Response(response=json.dumps(todos),
-                    mimetype="application/json") #Response 객체 생성
-    return resp
+    todos = db_session.query(Todos).all()
+    return jsonify(todos_schema.dump(todos).data)
 
 
 @app.route("/todos/<int:todo_id>", methods=['PUT'])
 def update_todo(todo_id):
-    db = get_db_connect()
-    c = db.cursor(MySQLdb.cursors.DictCursor)
+    todo = db_session.query(Todos).filter_by(id=todo_id).one()
+    todo.checked = 0 if todo.checked == 1 else 1
+    db_session.commit()
 
-    c.execute(f"""select * from todos where id = '{todo_id}'""")
-    todo = c.fetchone()
-    checked = 0 if todo['checked'] == 1 else 1
-    c.execute(f"""update todos set checked='{checked}' where id = '{todo_id}'""")
-    db.commit()
-
-    c.execute("""select id, todo as text, checked, UNIX_TIMESTAMP(created_at) as created_at from todos""")
-    todos = c.fetchall()
-    c.close()
-
-    resp = Response(response=json.dumps(todos),
-                    mimetype="application/json")  # Response 객체 생성
-    return resp
+    todos = db_session.query(Todos).all()
+    return jsonify(todos_schema.dump(todos).data)
 
 
 @app.route("/todos/<int:todo_id>", methods=['DELETE'])
 def remove_todo(todo_id):
-    db = get_db_connect()
-    c = db.cursor(MySQLdb.cursors.DictCursor)
+    db_session.query(Todos).filter_by(id=todo_id).delete()
+    db_session.commit()
 
-    c.execute(f"""delete from todos where id = {todo_id}""")
-    db.commit()
-
-    c.execute("""select id, todo as text, checked, UNIX_TIMESTAMP(created_at) as created_at from todos""")
-    todos = c.fetchall()
-    c.close()
-
-    resp = Response(response=json.dumps(todos),
-                    mimetype="application/json") #Response 객체 생성
-    return resp
+    todos = db_session.query(Todos).all()
+    return jsonify(todos_schema.dump(todos).data)
 
 
 if __name__ == "__main__":
