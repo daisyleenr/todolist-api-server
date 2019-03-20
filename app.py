b98e605c8e2d126c7f7222af71e0ced685934535
Nara Lee <daisyleenr@gmail.com>
2019-03-20 11:06:56 +0900

    Fix small bug

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from flask_cors import CORS
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from model import *
-from config import TEST_DB_STRING
+from model import Todos
+from config import DB_STRING
 from marshmallow import Schema, fields
 
-engine = create_engine(TEST_DB_STRING, echo=True, pool_recycle=3600)
+engine = create_engine(DB_STRING, echo=True, pool_recycle=3600)
 Session = sessionmaker(bind=engine)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
