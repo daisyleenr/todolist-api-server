c5569d35440784fae02c2213ecfd63839eb81a69
Nara Lee <daisyleenr@gmail.com>
2019-04-18 17:59:01 +0900

    Add mysql on travis

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 import os
 
-DB_USER = os.getenv("TODO_DB_USER", "todolist")
-DB_PASSWORD = os.getenv("TODO_DB_PASSWORD", "todolist")
+DB_USER = os.getenv("TODO_DB_USER", "root")
+DB_PASSWORD = os.getenv("TODO_DB_PASSWORD", "")
 DB_NAME = os.getenv("TODO_DB_NAME", "todolist")
 DB_HOST = os.getenv("TODO_DB_HOST", "localhost")
 DB_STRING = "mysql://" + DB_USER + ":" + DB_PASSWORD + "@" + DB_HOST + "/" + DB_NAME + "?charset=utf8"
